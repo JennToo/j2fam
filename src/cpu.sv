0f8ea292be711a1d383f835d790e8f4fcd7c68bf
Jennifer Wilcox <jennifer@nitori.org>
2022-04-13 19:09:54 -0500

    Start implementing the reset procedure
    I'm sure this is broken, but it's something I can start synthesizing and
    running simulations against.

--- src/cpu.sv
+++ src/cpu.sv
@@ -9,6 +9,7 @@ module cpu #(
 
     output logic [7:0] data_o,
     output logic [15:0] address_o,
+    output logic address_valid_o,
     output logic data_valid_o,
 
     // Testbench signals
@@ -16,9 +17,8 @@ module cpu #(
 );
 
   logic [7:0] clock_divider;
-
+  assign clock_ready_o = clock_ready;
   logic clock_ready;
-
   always_ff @(posedge clock_i) begin
     if (reset_i == 1) begin
       clock_ready   <= 0;
@@ -32,6 +32,41 @@ module cpu #(
     end
   end
 
-  assign clock_ready_o = clock_ready;
+  `define RESET_STAGE_1 6
+  `define RESET_STAGE_2 7
+
+  logic [ 2:0] instruction_stage;
+  logic [15:0] program_counter;
+  logic [ 7:0] current_instruction;
+
+  always_ff @(posedge clock_i) begin
+    if (reset_i == 1) begin
+      instruction_stage <= `RESET_STAGE_1;
+      address_valid_o <= 1;
+      address_o <= 16'hFFFC;
+    end else if (clock_ready) begin
+      case (instruction_stage)
+        `RESET_STAGE_1: begin
+          if (data_valid_i == 1) begin
+            instruction_stage <= `RESET_STAGE_2;
+            program_counter[7:0] <= data_i;
+            address_o <= 16'hFFFD;
+          end
+        end
+
+        `RESET_STAGE_2: begin
+          if (data_valid_i == 1) begin
+            instruction_stage <= 0;
+            program_counter[15:8] <= data_i;
+            address_o <= {data_i, program_counter[7:0]};
+            address_valid_o <= 1;
+          end
+        end
+
+        default begin
+        end
+      endcase
+    end
+  end
 
 endmodule : cpu
